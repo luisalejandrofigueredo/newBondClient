e1fdee8cf90d4e4878fd9463e8ca0e0d2f1cab5b
Luis Figueredo <luisalejandrofigueredo@gmail.com>
2022-11-11 19:12:36 -0300

    11/11/2022

--- src/app/interfaces/relations.ts
+++ src/app/interfaces/relations.ts
@@ -1,7 +1,8 @@
 import { Node  } from "./node";
 export interface Relations {
     id?:number,
+    name:string,
+    description:string,
     from:Node,
     to:Node,
-    description:string
 }
--- src/app/services/connections.service.ts
+++ src/app/services/connections.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { environment } from "../../environments/environment";
+import { Relations } from "../interfaces/relations";
 @Injectable({
   providedIn: 'root'
 })
 export class ConnectionsService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  getNodes(id: number): Promise<Relations[]> {
+    const options = {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json', 'Authorization': `Bearer: ${localStorage.getItem("token")}` }),
+      params: new HttpParams().append('id', id)
+    };
+    return new Promise<Relations[]>((resolve, reject) => {
+      this.httpClient.get<Relations[]>(`${environment.baseUrl}node/getAll`, options).subscribe((relations) => {
+        resolve(<Relations[]>relations);
+      }, (error) => { reject([]) });
+    })
+  }
 }
