daba1b72232b28e830c68fdf0eb036745cee50f3
Luis Figueredo <luisalejandrofigueredo@gmail.com>
2023-04-27 20:42:17 -0300

    27/04/2023

--- src/app/testgdi/testgdi/testgdi.component.ts
+++ src/app/testgdi/testgdi/testgdi.component.ts
@@ -11,12 +11,14 @@ export class TestgdiComponent implements AfterViewInit, OnInit {
   move = false;
   @ViewChild('canvas', { static: true }) canvas!: ElementRef<HTMLCanvasElement>;
   constructor() {
+    this.gd.start(800,600);
     this.gd.addNode({ x: 150, y: 150 }, "uno", 10, "number one", false, 10, 10);
     this.gd.addNode({ x: 50, y: 50 }, "dos", 10, "number two", false, 10, 10);
     this.gd.addConnection({ x: 150, y: 150 }, { x: 50, y: 50 }, "#000000")
     this.gd.addLabel(70, 70, "Hola Mundo", 20, 270);
     const connect = this.gd.getItem(3);
-    (connect as unknown as ConnectionObject).Name = "hello word connection";
+    (connect as unknown as ConnectionObject).Name = "hello word" ;
+    (connect as unknown as ConnectionObject).
   }
 
   ngAfterViewInit(): void {
